13b2ac05e519bc4c6adc8ce7c3ac67cafa145eb9
Léo Wadin <wadinleo@gmail.com>
2024-09-11 16:02:37 +0200

    chroe: remove unused imports

--- lib/search.dart
+++ lib/search.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/models/Station.dart';
 import 'package:provider/provider.dart';
 import 'widgets/date_picker_row.dart';
 import 'providers/StationProvider.dart';
